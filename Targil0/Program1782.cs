cc317e743485195f5959c2d9e7a8eefd78c9a1df
akivaab <amabramo18@gmail.com>
2021-10-10 13:59:29 +0300

    initial code

---
+++ Targil0/Program1782.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome1782();
+            Welcome1816();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome1816();
+
+        private static void Welcome1782()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
