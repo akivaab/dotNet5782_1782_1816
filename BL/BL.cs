f5117a5fb0790799fc0785760d33dd085887f5da
menachem kuszer <59207261+dentetorto@users.noreply.github.com>
2021-11-02 20:49:47 +0200

    erased extra files, set the IBL interface and  started the BS constructor

--- BL/BL.cs
+++ BL/BL.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using IBL.BO;
+using IDAL.DO;
 
 namespace IBL
 {
@@ -10,7 +11,122 @@ namespace IBL
 
         public BL()
         {
-            
+           
+            IDal.IDal dalObject = new DalObject.DalObject();
+            double[] powerConsumption = dalObject.DronePowerConsumption();
+            IEnumerable<Drone> enumerable = dalObject.DisplayDronesList();
+            drones = (List<Drone>)enumerable;
+
+        }
+
+        public void Addcustomer(int CustomeID, string Name, string Phone)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddDrone(int DroneID, int stationID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddPackage(int SenderID, int ReceiverId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddStation(int StationID, string Name, double latitude, double longitude, int numChargeSlots)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AssignPackageDrone(int DroneID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CollectPackage(int DroneID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeliveryPackage(int DroneID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisplayAllCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisplayAllDrones()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisplayAllPackage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisplayAllStation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisplayAllUnassignedPackages()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisplayCustomer(int CustomerID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisplayDrone(int DroneID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisplayFreeStations()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisplayPackage(int PackageID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisplayStation(int StationID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReleaseFromCharge(int DroneID, DateTime ChargingTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendDroneCharge(int DroneID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCustomer(int CustomerID, string Name, string phone)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateDroneName(int DroneID, string Name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateStation(int StationID, string Name, int NumberChargingSlots)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- BL/IBL.cs
+++ BL/IBL.cs
@@ -1,10 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 
+
 namespace IBL
 {
     public interface IBL
     {
+        public void AddStation(int StationID, string Name, double latitude, double longitude, int numChargeSlots);
 
+        //need to add weight
+        public void AddDrone(int DroneID ,int stationID);
+
+        public void Addcustomer(int CustomeID, string Name, string Phone);
+
+        //need add the weigh/status field
+        public void AddPackage(int SenderID ,int ReceiverId );
+
+        public void UpdateDroneName(int DroneID, string Name);
+
+        public void UpdateStation(int StationID, string Name, int NumberChargingSlots );
+
+        public void UpdateCustomer(int CustomerID, string Name, string phone);
+
+        public void SendDroneCharge(int DroneID);
+
+        public void ReleaseFromCharge(int DroneID, DateTime ChargingTime);
+
+        public void AssignPackageDrone(int DroneID);
+
+        public void CollectPackage(int DroneID);
+
+        public void DeliveryPackage(int DroneID);
+
+        public void DisplayStation(int StationID);
+
+        public void DisplayDrone(int DroneID);
+
+        public void DisplayCustomer(int CustomerID);
+
+        public void DisplayPackage(int PackageID);
+
+        public void DisplayAllStation();
+
+        public void DisplayAllDrones();
+
+        public void DisplayAllCustomers();
+
+        public void DisplayAllPackage();
+
+        public void DisplayAllUnassignedPackages();
+
+        public void DisplayFreeStations();
     }
 }
