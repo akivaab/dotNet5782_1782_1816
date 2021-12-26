e0d297fd4ecbfc53c1cf07d306a4994e52672ce3
akivaab <amabramo18@gmail.com>
2021-12-26 20:39:42 +0200

    let the UI begin

--- PL/MainWindow.xaml.cs
+++ PL/MainWindow.xaml.cs
@@ -26,12 +26,20 @@ namespace PL
         BlApi.IBL bl;
 
         /// <summary>
+        /// Collection of usernames and passwords.
+        /// </summary>
+        IEnumerable<string> usernames;
+        IEnumerable<string> passwords;
+
+        /// <summary>
         /// MainWindow constructor that gets a BL instance.
         /// </summary>
         public MainWindow()
         {
             InitializeComponent();
             bl = BlApi.BlFactory.GetBl();
+            usernames = new List<string>();
+            passwords = new List<string>();
         }
 
         /// <summary>
@@ -42,6 +50,47 @@ namespace PL
         private void ShowDroneListButton_Click(object sender, RoutedEventArgs e)
         {
             new DroneListWindow(bl).Show();
+        }
+
+        /// <summary>
+        /// Verify an employee signing in.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void employeeSignIn_Click(object sender, RoutedEventArgs e)
+        {
+            if (employeeName.Text == "WDGaster" && employeePassword.Password == "WingDings")
+            {
+                login.Visibility = Visibility.Collapsed;
+                employeeView.Visibility = Visibility.Visible;
+            }
+        }
+
+        /// <summary>
+        /// Verify a customer signing in.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void customerSignIn_Click(object sender, RoutedEventArgs e)
+        {
+            if (usernames.Contains(customerName.Text) && passwords.Contains(employeePassword.Password))
+            {
+                login.Visibility = Visibility.Collapsed;
+                customerView.Visibility = Visibility.Visible;
+            }
+        }
+
+        /// <summary>
+        /// Register a new customer.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void register_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO: open CustomerRegisterWindow to add new customer
+
+            login.Visibility = Visibility.Collapsed;
+            customerView.Visibility = Visibility.Visible;
         }
     }
 }
