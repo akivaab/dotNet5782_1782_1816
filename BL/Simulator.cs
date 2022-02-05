7b691fe348c08ef40089e02969aa2b13da445f47
akivaab <amabramo18@gmail.com>
2022-02-05 22:14:23 +0200

    defined simulation steps

--- BL/Simulator.cs
+++ BL/Simulator.cs
@@ -15,7 +15,7 @@ namespace BL
         /// <summary>
         /// Delay between checks for the drone to perform actions.
         /// </summary>
-        private const int timerDelay = 500;
+        private const int delay = 500;
 
         /// <summary>
         /// Speed of a drone, in kilometers per second.
@@ -23,9 +23,29 @@ namespace BL
         private const double droneSpeed = 30;
 
         /// <summary>
+        /// The drone whose duties are being simulated.
+        /// </summary>
+        private Drone drone;
+
+        /// <summary>
+        /// Instance of the BL layer.
+        /// </summary>
+        private BlApi.IBL bl;
+
+        /// <summary>
         /// Collection of names of the windows that need to be updated.
         /// </summary>
         private IEnumerable<string> windowsToUpdate;
+
+        /// <summary>
+        /// An action which updates the windows displayed in the application when invoked.
+        /// </summary>
+        private Action<int, IEnumerable<string>> updateDisplay;
+
+        /// <summary>
+        /// Value marking whether to end the simulation.
+        /// </summary>
+        private bool endSimulation = false;
 
         /// <summary>
         /// Simulator constructor, runs the simulation.
@@ -36,12 +56,91 @@ namespace BL
         /// <param name="stop"></param>
         public Simulator(BlApi.IBL bl, int droneID, Action<int, IEnumerable<string>> updateDisplay, Func<bool> stop)
         {
-            while(!stop())
+            this.bl = bl;
+            this.updateDisplay = updateDisplay;
+            while(!(stop() || endSimulation))
             {
                 try
                 {
-                    bl.AssignPackage(droneID);
+                    drone = bl.GetDrone(droneID);
+                    Thread.Sleep(delay);
+                    switch (drone.Status)
+                    {
+                        case Enums.DroneStatus.available:
+                            assign();
+                            break;
+                        case Enums.DroneStatus.maintenance:
+                            charge();
+                            break;
+                        case Enums.DroneStatus.delivery:
+                            deliver();
+                            break;
+                    }
                 }
+                catch (UndefinedObjectException)
+                {
+
+                }
+                catch (XMLFileLoadCreateException)
+                {
+
+                }
+            }
+        }
+
+        private void assign()
+        {
+            try
+            {
+                bl.AssignPackage(drone.ID);
+            }
+            catch (EmptyListException e)
+            {
+                if (e.Rectifiable)
+                {
+                    bl.SendDroneToCharge(drone.ID);
+                }
+                else
+                {
+                    endSimulation = true;
+                }
+            }
+            updateDisplay.Invoke();
+        }
+
+        private void charge()
+        {
+            try
+            {
+                if (drone.Battery == 100)
+                {
+                    DateTime beganCharging = bl.GetTimeChargeBegan(drone.ID);
+                    bl.ReleaseFromCharge(drone.ID, (DateTime.Now - beganCharging).TotalSeconds);
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void deliver()
+        {
+            try
+            {
+                Package package = bl.GetPackage(drone.PackageInTransfer.ID);
+
+                if (package.CollectingTime == null)
+                {
+                    bl.CollectPackage(drone.ID);
+                }
+                else if (package.DeliveringTime == null)
+                {
+                    bl.DeliverPackage(drone.ID);
+                }
+            }
+            catch
+            {
 
             }
         }
